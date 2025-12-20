a1e8710f3fc124c8e2e5bd4c9bd0062cb16c1acd
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-20 15:02:11 +0530

    [Bhumika Yadav] .ADD 2 scenario based problems on Day5

---
+++ core-java-practice/scenario-based/CoffeeCounterChronicles.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class CoffeeCounterChronicles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking number of customers using variable customer
+        System.out.print("Enter the number of customers: ");
+        int customer = sc.nextInt();
+
+        double GST = 0.15;
+
+        // Taking counter variable and initializing it 1 to keep track of number of customers
+        int COUNTER = 1;
+
+        // Using while loop to keep track of the customers and taking the order for each customer. As soon as the counter becomes greater than the customer count the loop terminates
+        while(COUNTER <= customer) {
+            System.out.println("Take the order for customer " + COUNTER);
+
+            // Taking coffee type order from the customer
+            System.out.print("Enter the type of coffee (Espresso/Latte/Cappuccino or exit to stop):");
+            String typeCoffee = sc.next();
+
+            // To check for exit statement. If exit is entered by the user then loop breaks
+            if(typeCoffee.equalsIgnoreCase("exit")){
+                System.out.println("Cafe is closed!");
+                break;
+            }
+
+            // Taking the quantity of coffee orderd by the customer
+            System.out.print("Enter the quantity of coffee: ");
+            int quantity = sc.nextInt();
+
+            // Making the switch case to display the price of different coffee types ordered by the customer
+            double price = 0.0;
+            switch(typeCoffee.toUpperCase()) {
+                case "ESPRESSO":
+                    price = 120;
+                    System.out.println("The price for Espresso is INR " + price);
+                    break;
+                case "LATTE":
+                    price = 150;
+                    System.out.println("The price for Latte is INR " + price);
+                    break;
+                case "CAPPUCCINO":
+                    price = 180;
+                    System.out.println("The price for Cappuccino is INR " + price);
+                    break;
+                default:
+                    System.out.println("Please enter the coffee type again");
+                    continue; // Skip incrementing counter to retry for the same customer
+            }
+
+            // Finding the original price of the order
+            double total = price * quantity;
+
+            // Finding the gst applied to the particular order
+            double gst = total * GST;
+
+            // Finding the total bill to be paid
+            double totalBill = total + gst;
+
+            System.out.println("Your order amount is INR " + total);
+            System.out.println("GST included is INR " + gst);
+            System.out.println("The total bill to be paid is INR " + totalBill);
+            COUNTER++;
+        }
+    }
+}
---
+++ core-java-practice/scenario-based/MayaBMI.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class MayaBMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking clients using variable client
+        System.out.print("Enter number of clients: ");
+        int client = sc.nextInt(); 
+        
+        // Initializing counter variable to keep track of number of clients
+        int COUNTER = 1;
+
+        // Here we are using while loop to track the number of clients and for each client taking his/her weight and height. As soon as counter value becomes greater than client the loop will terminate
+        while(COUNTER <= client) {
+            System.out.println("\nEnter the details of for client " + COUNTER + ":");
+            
+            System.out.print("Enter weight in kilograms: ");
+            double weight = sc.nextDouble();
+
+            System.out.print("Enter height in centimeters: ");
+            double height = sc.nextDouble();
+
+            // Converting height from centimeters to meters
+            height = height / 100;
+
+            // Calculating BMI using the formula
+            double bmi = weight / (height * height);
+            System.out.printf("Your BMI is %.2f", bmi);
+
+            // Checking the BMI category using conditional statements
+            if(bmi < 18.5) {
+                System.out.println(" and you are Underweight");
+            } else if(bmi >= 18.5 && bmi < 24.9) {
+                System.out.println(" and you have Normal weight");
+            } else if(bmi >= 25 && bmi < 29.9) {
+                System.out.println(" and you are Overweight");
+            } else {
+                System.out.println(" and you are Obese");
+            }
+            COUNTER++; // Incrementing counter each time his/her BMI is calculated
+        }
+    }
+}
