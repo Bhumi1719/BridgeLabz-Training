491af2a6dab83dab0b1cc431436a0a3e51718ab7
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-25 22:09:26 +0530

    [Bhumika Yadav] .ADD added 2 problems of scenario based questions on Day 9

---
+++ core-java-practice/scenario-based/ElectionBoothManager.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class ElectionBoothManager {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Initializing 0 votes for 1
+        int VOTE1 = 0;
+        // Initializing 0 votes for 2
+        int VOTE2 = 0;
+        // Initializing 0 votes for 3
+        int VOTE3 = 0;
+
+        // Running an infinite loop for multiple voters which ends when special character is entered
+        while(true) {
+            System.out.print("\nEnter your age or enter -1 to exit: ");
+            int age = sc.nextInt(); // Age is taken user input inside the loop to take input for multiple voters
+
+            if(age == -1) {
+                System.out.println("Voting is closed"); // Edge case for loop to terminate
+                break;
+            }
+            if(age >= 18) {
+                System.out.println("Vote for 1/2/3: ");
+                int vote = sc.nextInt(); // If age >= 18 then eligible for vote and give vote for any three
+
+                // Conditionals for counting number of votes
+                if(vote == 1) {
+                    VOTE1 ++;
+                } else if(vote == 2) {
+                    VOTE2 ++;
+                } else if(vote == 3) {
+                    VOTE3 ++;
+                } else {
+                    System.out.println("Give proper vote");
+                }
+            } else {
+                System.out.println("Not eligible for vote");
+            }
+        }
+
+        System.out.println("\nResult of Elections are: ");
+        System.out.println("Total votes given to 1 candidate: " + VOTE1);
+        System.out.println("Total votes given to 2 candidate: " + VOTE2);
+        System.out.println("Total votes given to 3 candidate: " + VOTE3);
+    } 
+}
