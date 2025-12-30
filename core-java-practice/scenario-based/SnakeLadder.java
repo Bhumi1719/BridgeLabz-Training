7e6b6ec74c8efef44a67fcd2534b03d38e06e993
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-30 15:34:12 +0530

    [Bhumika Yadav] .ADD adding USECASE-3 for Snake and Ladder problem on Day 14

--- core-java-practice/scenario-based/SnakeLadder.java
+++ core-java-practice/scenario-based/SnakeLadder.java
@@ -11,7 +11,23 @@ public class SnakeLadder {
         Random random = new Random(); // Creating an object of random
         int roll = random.nextInt(6) + 1; // Generating numbers between 1 to 6 randomly
 
-        System.out.println("The number appeared on dice after rolling is: " + roll);
+        System.out.println("\nThe number appeared on dice after rolling is: " + roll);
+
+        int option = random.nextInt(3); // Generating an option for the player to play the game
+        // option = 0 -> No play
+        // option = 1 -> Ladder
+        // option = 2 -> Snake
+        if(option == 0) {
+            System.out.println("\nNo Play");
+            System.out.print("Position is " + POSITION);
+        } else if(option == 1) {
+            System.out.println("\nLadder");
+            System.out.print("Position is: " + (POSITION += roll));
+        } else {
+            System.out.println("\nSnake");
+            System.out.print("Position is: " + (POSITION -= roll));
+        }
+
         
     }
 }
