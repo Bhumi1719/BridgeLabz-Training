fc833c574169f916c0c38396940db61fd84f35da
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-30 16:16:29 +0530

    [Bhumika Yadav] .ADD adding USECASE-6 for Snake and Ladder problem on Day 14

--- core-java-practice/scenario-based/SnakeLadder.java
+++ core-java-practice/scenario-based/SnakeLadder.java
@@ -7,12 +7,14 @@ public class SnakeLadder {
         System.out.println("Welcome to the Snake and Ladder Game with Single Player");
 
         int POSITION = 0; // Position of the player in the starting
+        int DICE_ROLL = 0; // Variable to store number of times the dice was rolled
 
         Random random = new Random(); // Creating an object of random
 
         // Loop executes until the player position reaches 100
         while(POSITION < 100) {
             int roll = random.nextInt(6) + 1; // Generating numbers between 1 to 6 randomly
+            DICE_ROLL ++; // Increamenting the count of dice roll after each roll
 
             System.out.println("\nThe number appeared on dice after rolling is: " + roll);
 
@@ -23,19 +25,19 @@ public class SnakeLadder {
             if(option == 0) {
 
                 System.out.println("\nNo Play Condition");
-                System.out.print("Position is " + POSITION);
+                System.out.print("Position is " + POSITION); // Specifying the position after each move
 
             } else if(option == 1) {
                 if(POSITION + roll <= 100) { // Condiiton for exactly reaching 100
                     POSITION += roll;
                 }
                 System.out.println("\nLadder Condition");
-                System.out.print("Position is: " + POSITION);
+                System.out.print("Position is: " + POSITION); // Specifying the position after each move
 
             } else {
 
                 System.out.println("\nSnake Condition");
-                System.out.print("Position is: " + (POSITION -= roll));
+                System.out.print("Position is: " + (POSITION -= roll)); // Specifying the position after each move
 
 
                 if(POSITION < 0) { // If player reaches below 0 then it restarts from the0 position
@@ -44,5 +46,7 @@ public class SnakeLadder {
             }
 
         }        
+
+        System.out.println("\nNumber of times the dice rolled is: " + DICE_ROLL);
     }
 }
