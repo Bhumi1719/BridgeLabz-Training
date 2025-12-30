73a92e39371fa14ff1d1e8a99af16ed11114718e
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-30 15:39:06 +0530

    [Bhumika Yadav] .ADD adding USECASE-4 for Snake and Ladder problem on Day 14

--- core-java-practice/scenario-based/SnakeLadder.java
+++ core-java-practice/scenario-based/SnakeLadder.java
@@ -9,25 +9,38 @@ public class SnakeLadder {
         int POSITION = 0; // Position of the player in the starting
 
         Random random = new Random(); // Creating an object of random
-        int roll = random.nextInt(6) + 1; // Generating numbers between 1 to 6 randomly
 
-        System.out.println("\nThe number appeared on dice after rolling is: " + roll);
+        // Loop executes until the player position reaches 100
+        while(POSITION < 100) {
+            int roll = random.nextInt(6) + 1; // Generating numbers between 1 to 6 randomly
 
-        int option = random.nextInt(3); // Generating an option for the player to play the game
-        // option = 0 -> No play
-        // option = 1 -> Ladder
-        // option = 2 -> Snake
-        if(option == 0) {
-            System.out.println("\nNo Play");
-            System.out.print("Position is " + POSITION);
-        } else if(option == 1) {
-            System.out.println("\nLadder");
-            System.out.print("Position is: " + (POSITION += roll));
-        } else {
-            System.out.println("\nSnake");
-            System.out.print("Position is: " + (POSITION -= roll));
-        }
+            System.out.println("\nThe number appeared on dice after rolling is: " + roll);
 
-        
+            int option = random.nextInt(3); // Generating an option for the player to play the game
+            // option = 0 -> No play
+            // option = 1 -> Ladder
+            // option = 2 -> Snake
+            if(option == 0) {
+
+                System.out.println("\nNo Play Condition");
+                System.out.print("Position is " + POSITION);
+
+            } else if(option == 1) {
+
+                System.out.println("\nLadder Condition");
+                System.out.print("Position is: " + (POSITION += roll));
+
+            } else {
+
+                System.out.println("\nSnake Condition");
+                System.out.print("Position is: " + (POSITION -= roll));
+
+
+                if(POSITION < 0) { // If player reaches below 0 then it restarts from the0 position
+                    POSITION = 0;
+                }
+            }
+
+        }        
     }
 }
