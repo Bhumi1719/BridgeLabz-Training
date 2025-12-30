701edc67515732d7df6a7d159e2d5e93a2e21b7d
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-30 16:49:37 +0530

    [Bhumika Yadav] .ADD adding USECASE-7 for Snake and Ladder problem on Day 14

--- core-java-practice/scenario-based/SnakeLadder.java
+++ core-java-practice/scenario-based/SnakeLadder.java
@@ -6,47 +6,97 @@ public class SnakeLadder {
 
         System.out.println("Welcome to the Snake and Ladder Game with Single Player");
 
-        int POSITION = 0; // Position of the player in the starting
+        int POSITION_1 = 0; // Position of the player 1 in the starting
+        int POSITION_2 = 0; // Position of the player 2 in the starting
         int DICE_ROLL = 0; // Variable to store number of times the dice was rolled
+        int TURN = 0; // 0 -> player1 and 1 -> player2
 
         Random random = new Random(); // Creating an object of random
 
         // Loop executes until the player position reaches 100
-        while(POSITION < 100) {
+        while(POSITION_1 < 100 && POSITION_2 < 100) {
             int roll = random.nextInt(6) + 1; // Generating numbers between 1 to 6 randomly
             DICE_ROLL ++; // Increamenting the count of dice roll after each roll
-
-            System.out.println("\nThe number appeared on dice after rolling is: " + roll);
 
             int option = random.nextInt(3); // Generating an option for the player to play the game
             // option = 0 -> No play
             // option = 1 -> Ladder
             // option = 2 -> Snake
-            if(option == 0) {
 
-                System.out.println("\nNo Play Condition");
-                System.out.print("Position is " + POSITION); // Specifying the position after each move
+            if(TURN == 0) { // Condition for player 1 chance
+                System.out.println("\nTurn of Player 1");
+                System.out.println("\n\nThe number appeared on dice after rolling by Player 1 is: " + roll);
 
-            } else if(option == 1) {
-                if(POSITION + roll <= 100) { // Condiiton for exactly reaching 100
-                    POSITION += roll;
+                if(option == 0) {
+
+                    System.out.println("\nNo Play Condition");
+                    System.out.println("Player 1 Position " + POSITION_1); // Specifying the position after each move
+                    System.out.print("Player 2 Position: " + POSITION_2);
+                } else if(option == 1) {
+                    if(POSITION_1 + roll <= 100) { // Condiiton for exactly reaching 100
+                        POSITION_1 += roll;
+                    }
+                    System.out.println("\nLadder Condition");
+                    System.out.println("Player 1 Position " + POSITION_1); // Specifying the position after each move
+                    System.out.print("Player 2 Position: " + POSITION_2);
+                    
+                } else {
+                    POSITION_1 -= roll;
+                    System.out.println("\nSnake Condition");
+                    System.out.println("Player 1 Position " + POSITION_1); // Specifying the position after each move
+                    System.out.print("Player 2 Position: " + POSITION_2);
+
+
+                    if(POSITION_1 < 0) { // If player reaches below 0 then it restarts from the0 position
+                        POSITION_1 = 0;
+                    }
                 }
-                System.out.println("\nLadder Condition");
-                System.out.print("Position is: " + POSITION); // Specifying the position after each move
+
+                // Change turn ONLY if not Ladder
+                if (option != 1) {
+                    TURN = 1;   // Player 2's turn
+                }
 
             } else {
+                System.out.println("\nTurn of Player 2");
+                System.out.println("\n\nThe number appeared on dice after rolling by Player 2 is: " + roll);
 
-                System.out.println("\nSnake Condition");
-                System.out.print("Position is: " + (POSITION -= roll)); // Specifying the position after each move
+                if(option == 0) {
+
+                    System.out.println("\nNo Play Condition");
+                    System.out.println("Player 1 Position " + POSITION_1); // Specifying the position after each move
+                    System.out.print("Player 2 Position: " + POSITION_2);
+
+                } else if(option == 1) {
+                    if(POSITION_2 + roll <= 100) { // Condiiton for exactly reaching 100
+                        POSITION_2 += roll;
+                    }
+                    System.out.println("\nLadder Condition");
+                    System.out.println("Player 1 Position " + POSITION_1); // Specifying the position after each move
+                    System.out.print("Player 2 Position: " + POSITION_2);
+
+                } else {
+
+                    System.out.println("\nSnake Condition");
+                    System.out.println("Player 1 Position " + POSITION_1); // Specifying the position after each move
+                    System.out.print("Player 2 Position: " + POSITION_2);
 
 
-                if(POSITION < 0) { // If player reaches below 0 then it restarts from the0 position
-                    POSITION = 0;
+                    if(POSITION_2 < 0) { // If player reaches below 0 then it restarts from the0 position
+                        POSITION_2 = 0;
+                    }
                 }
+                if(option != 1) {
+                    TURN = 0; // Player 1 turn
+                } 
             }
-
         }        
 
-        System.out.println("\nNumber of times the dice rolled is: " + DICE_ROLL);
+        System.out.println("\n\nNumber of times the dice rolled is: " + DICE_ROLL);
+
+        if (POSITION_1 == 100)
+            System.out.println("Player 1 Wins!");
+        else
+            System.out.println("Player 2 Wins!");
     }
 }
