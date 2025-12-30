974a7034766f511b07b64e5d0827c816abd69f58
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2025-12-30 15:23:18 +0530

    [Bhumika Yadav] .ADD adding USECASE-1 for Snake and Ladder problem on Day 14

---
+++ core-java-practice/scenario-based/SnakeLadder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Snake and Ladder Game with Single Player");
+
+        int POSITION = 0; // Position of the player in the starting
+
+        System.out.println("The position of player at the starting of the game is: " + POSITION);
+        
+    }
+}
