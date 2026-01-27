ed1a5953ff53907de2b76eb938121cf0c5179ec2
Bhumika Yadav <122299690+Bhumi1719@users.noreply.github.com>
2026-01-27 11:44:49 +0530

    [Bhumika Yadav] .ADD adding UC-5 and Uc-6 of Address Book Problem n Day 42

--- oops-practice/gcr-codebase/address-book-problem/AddressProblem.java
+++ oops-practice/gcr-codebase/address-book-problem/AddressProblem.java
@@ -2,27 +2,41 @@ import java.util.*;
 
 public class AddressProblem {
 
+	static Scanner sc = new Scanner(System.in);
+	static Map<String, Contacts> addressBooks = new HashMap<>();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		Contacts contact = new Contacts();
 		
 		System.out.println("Welcome to Address Book Program");
 		
 		while(true) {
-			System.out.println("\n1. Add Contact");
-			System.out.println("2. Display Contact");
-			System.out.println("3. Edit Contact");
-			System.out.println("4. Delete Contact");
-			System.out.println("5. Exit");
+			System.out.println("\n1. Add Address Book");
+			System.out.println("2. Add Contact");
+			System.out.println("3. Display Contact");
+			System.out.println("4. Edit Contact");
+			System.out.println("5. Delete Contact");
+			System.out.println("6. Exit");
 			
 			System.out.print("\nEnter your choice: ");
 			int choice = sc.nextInt();
-			sc.nextLine()
-;			
+			sc.nextLine();
+
 			switch(choice) {
-				case 1 : 
+				case 1 :
+					System.out.print("Enter Address Book Name: ");
+                    String bookName = sc.nextLine();
+
+                    if(addressBooks.containsKey(bookName)) {
+                        System.out.println("Address Book already exists");
+                    } else {
+                        addressBooks.put(bookName, new Contacts());
+                        System.out.println("Address Book created successfully");
+                    }
+                    break;
+
+				case 2 : 
+					Contacts contact = getAddressBook();
+                    if(contact == null) break;
+
 					System.out.print("Enter the first name: ");
 					String firstName = sc.nextLine();
 					
@@ -53,11 +67,17 @@ public class AddressProblem {
 					contact.addContact(contacts);
 					break;
 					
-				case 2 :
-					contact.displayContact();
-					break;
-					
 				case 3 :
+					contact = getAddressBook();
+                    if(contact != null) {
+                        contact.displayContact();
+                    }
+                    break;
+					
+				case 4 :
+					contact = getAddressBook();
+                    if (contact == null) break;
+
 					System.out.print("Enter the first name: ");
 					String searchFirstName = sc.nextLine();
 					
@@ -67,7 +87,10 @@ public class AddressProblem {
 					contact.editContact(searchFirstName, searchLastName, sc);
 					break;
 					
-				case 4 :
+				case 5 :
+					contact = getAddressBook();
+                    if (contact == null) break;
+
 					System.out.print("Enter the fisrt name: ");
 					searchFirstName = sc.nextLine();
 					
@@ -77,7 +100,7 @@ public class AddressProblem {
 					contact.deleteContact(searchFirstName, searchLastName);
 					break;
 					
-				case 5 :
+				case 6 :
 					System.out.println("Exited");
 					System.exit(0);
 					
@@ -88,5 +111,16 @@ public class AddressProblem {
 		}
 		
 	}
+
+	private static Contacts getAddressBook() {
+        System.out.print("Enter Address Book Name: ");
+        String name = sc.nextLine();
+
+        Contacts contact = addressBooks.get(name);
+        if(contact == null) {
+            System.out.println("Address Book not found");
+        }
+        return contact;
+    }
 
 }
